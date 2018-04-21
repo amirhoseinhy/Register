9567b18f9ae63a7cafcd393ed5d9fd8b83951a1c
Amirhosein Heydari <mramir2008@gmail.com>
2018-04-21 20:42:00 +0430

    Initial commit

---
+++ app/src/main/java/com/niksan/register/URLs.java
@@ -0,0 +1,10 @@
+package com.niksan.register;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://192.168.43.200/android/api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
---
+++ app/src/main/java/com/niksan/register/User.java
@@ -0,0 +1,35 @@
+package com.niksan.register;
+
+public class User {
+
+    private String username, email, gender;
+    private int id;
+
+
+    public User(int id, String username, String email, String gender) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
